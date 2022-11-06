bf01c4360afedb07f64ae64a0666766e7f65c9d3
dubeyshreyas <68475604+dubeyshreyas@users.noreply.github.com>
2022-11-06 23:02:42 +0530

    first commit

---
+++ client/build/precache-manifest.72d454275a4804827a6e02bc5e6f5d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd92ef6e2431dca84efc9b9ab133f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af83a3c0c3888acd6427",
+    "url": "/static/css/main.74ae4e55.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af83a3c0c3888acd6427",
+    "url": "/static/js/main.58322e6f.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
